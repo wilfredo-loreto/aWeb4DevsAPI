4ab824646f42ea0206e7978995d97320f0216bda
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-09-18 19:04:30 -0400

    updated doc folder

--- doc/api_project.js
+++ doc/api_project.js
@@ -2,13 +2,13 @@ define({
   "name": "aWeb4Devs",
   "title": "aweb4devs.com",
   "version": "1.0.0",
-  "description": "aWeb4Devs API",
+  "description": "aWeb4Devs API to make our website totally dynamic and CRUD operations for updating it week-by-week",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-09-18T22:43:03.998Z",
+    "time": "2020-09-18T23:04:16.166Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
