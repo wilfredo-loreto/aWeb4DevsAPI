94ff708588e8e2fe15132c5ee0b892cee7ce1e80
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-09-26 21:51:11 -0400

    Something goes wrong

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 const app = express();
 const api = require('./routes/routes')
 
-app.use(express.static('public'))
+app.use(express.static('public') )
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
