9d9129b1f31cf8d4a06605278e845f1803434bee
Wilfredo Loreto <pipeloreto99@gmail.com>
2020-11-04 21:45:29 -0400

    .

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
     SECRET_PASSWORD: "xHz6#Rd 8z9D6 X3!2dx9x 6z/A*9s6e f96HoK>8sz16f#Hkp3",
-    port:process.env.port || 3800,
+    port:process.env.port || 3700, 
     uri:'mongodb+srv://wilfredo:aweb4devs19992020@cluster0.gqdek.mongodb.net/prueba1?retryWrites=true&w=majority',
     UPDATE_VISITS_PASSWORD:"Add 1 Visit to Our WebSite" 
 }
